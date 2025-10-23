5341085c7b61d9438b43d5c5a32fd3903b194d73
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-24 00:05:31 +0200

    Make target point coordinates available globally.
    The first step to guiding the motor into the right position is to
    make sure that the coordinates of the point of interest is known to
    the whole of the thread - which makes it a known for the lambda motor
    callback functions as well.

---
+++ include/solution/solution.h
@@ -0,0 +1,9 @@
+#ifndef MY_SOLUTION
+#define MY_SOLUTION
+typedef struct MyPoint {
+	double x;
+	double y;
+	double z;
+} MyPoint;
+
+#endif
--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -4,7 +4,13 @@
 #include <iostream>
 #include <thread>
 
+#include "../../include/solution/solution.h"
+
 int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
+	MyPoint target;
+	target.x = 0;
+	target.y = 0;
+	target.z = 0;
 
   std::cout << (preempt ? "Preempt" : "Queue") << '\n';
 
@@ -15,11 +21,15 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
   motor1->add_data_callback([](const uint16_t& data) {
     std::cout << "Motor 1 data: " << static_cast<int>(data) << "\n";
   });
-  motor2->add_data_callback([](const uint16_t& data) {
+  motor2->add_data_callback([&target](const uint16_t& data) {
     std::cout << "Motor 2 data: " << static_cast<int>(data) << "\n";
+    printf("Point's x is %lf\n",target.x);
   });
-  commands->add_data_callback([](const Point& point) {
+  commands->add_data_callback([&target](const Point& point) {
     std::cout << "Command point: (" << point.x << ", " << point.y << ", " << point.z << ")\n";
+    target.x = point.x;
+    target.y = point.y;
+    target.z = point.z;
   });
 
   std::this_thread::sleep_for(std::chrono::milliseconds(3600 * 1000));
